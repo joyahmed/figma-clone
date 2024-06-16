da26fe4b70a811da8e03310c30e8555510536f61
joyahmed <joy_ahmed_007@yahoo.com>
2024-06-16 11:32:08 +0600

    ✅Real Time Feature

--- app/page.tsx
+++ app/page.tsx
@@ -4,12 +4,19 @@ import LeftSidebar from '@/components/LeftSidebar';
 import Live from '@/components/Live';
 import Navbar from '@/components/Navbar';
 import RightSidebar from '@/components/RightSidebar';
+import { defaultNavElement } from '@/constants';
 import {
 	handleCanvasMouseDown,
+	handleCanvasMouseUp,
+	handleCanvasObjectModified,
+	handleCanvaseMouseMove,
 	handleResize,
-	initializeFabric
+	initializeFabric,
+	renderCanvas
 } from '@/lib/canvas';
+import { handleDelete } from '@/lib/key-events';
 import { ActiveElement } from '@/types/type';
+import { useMutation, useStorage } from '@liveblocks/react/suspense';
 import { fabric } from 'fabric';
 import { useEffect, useRef, useState } from 'react';
 
@@ -18,7 +25,21 @@ const Home = () => {
 	const fabricRef = useRef<fabric.Canvas | null>(null);
 	const isDrawing = useRef(false);
 	const shapeRef = useRef<fabric.Object | null>(null);
-	const selectedShapeRef = useRef<string | null>(null);
+	const selectedShapeRef = useRef<string | null>('');
+	const activeObjectRef = useRef<fabric.Object | null>();
+
+	const canvasObjects = useStorage(root => root.canvasObjects);
+
+	const syncShapeInStorage = useMutation(({ storage }, object) => {
+		if (!object) return;
+
+		const { objectId } = object;
+		const shapeData = object.toJSON();
+		shapeData.objectId = objectId;
+		const canvasObjects = storage.get('canvasObjects');
+		canvasObjects.set(objectId, shapeData);
+	}, []);
+
 	const [activeElement, setActiveElement] = useState<ActiveElement>({
 		name: '',
 		value: '',
@@ -38,13 +59,91 @@ const Home = () => {
 			});
 		});
 
+		canvas.on('mouse:move', options => {
+			handleCanvaseMouseMove({
+				options,
+				canvas,
+				isDrawing,
+				shapeRef,
+				selectedShapeRef,
+				syncShapeInStorage
+			});
+		});
+
+		canvas.on('mouse:up', options => {
+			handleCanvasMouseUp({
+				options,
+				canvas,
+				isDrawing,
+				shapeRef,
+				selectedShapeRef,
+				syncShapeInStorage,
+				setActiveElement,
+				activeObjectRef
+			});
+		});
+
+		canvas.on('object:modified', options => {
+			handleCanvasObjectModified({
+				options,
+				syncShapeInStorage
+			});
+		});
+
 		window.addEventListener('resize', () => {
 			handleResize({ canvas: fabricRef.current });
 		});
+
+		return () => {
+			canvas.dispose();
+		};
+	}, [syncShapeInStorage]);
+
+	useEffect(() => {
+		renderCanvas({
+			fabricRef,
+			canvasObjects,
+			activeObjectRef
+		});
+	}, [canvasObjects]);
+
+	const deleteAllShapes = useMutation(({ storage }) => {
+		const canvasObjects = storage.get('canvasObjects');
+
+		if (!canvasObjects || canvasObjects.size === 0) return true;
+		for (const [key, value] of canvasObjects.entries()) {
+			canvasObjects.delete(key);
+		}
+		return canvasObjects.size === 0;
 	}, []);
+
+	const deleteShapeFromStorage = useMutation(
+		({ storage }, objectId) => {
+			const canvasObjects = storage.get('canvasObjects');
+
+			canvasObjects.delete(objectId);
+		},
+		[]
+	);
 
 	const handleActiveElement = (elem: ActiveElement) => {
 		setActiveElement(elem);
+
+		switch (elem?.value) {
+			case 'reset':
+				deleteAllShapes();
+				fabricRef.current?.clear();
+				setActiveElement(defaultNavElement);
+				break;
+			case 'delete':
+				handleDelete(
+					fabricRef.current as any,
+					deleteShapeFromStorage
+				);
+				setActiveElement(defaultNavElement);
+			default:
+				break;
+		}
 
 		selectedShapeRef.current = elem?.value as string;
 	};
--- components/Room.tsx
+++ components/Room.tsx
@@ -1,19 +1,31 @@
 'use client';
 
+import { LiveMap } from '@liveblocks/client';
 import {
 	ClientSideSuspense,
 	LiveblocksProvider,
 	RoomProvider
 } from '@liveblocks/react/suspense';
 import { ReactNode } from 'react';
+import Loader from './Loader';
 
 export function Room({ children }: { children: ReactNode }) {
 	return (
 		<LiveblocksProvider
 			publicApiKey={process.env.NEXT_PUBLIC_LIVEBLOCKS_PUBLIC_KEY!}
 		>
-			<RoomProvider id='my-room' initialPresence={{}}>
-				<ClientSideSuspense fallback={<div>Loading…</div>}>
+			<RoomProvider
+				id='my-room'
+				initialPresence={{
+					cursor: null,
+					cursorColor: null,
+					editingText: null
+				}}
+				initialStorage={{
+					canvasObjects: new LiveMap()
+				}}
+			>
+				<ClientSideSuspense fallback={<Loader />}>
 					{children}
 				</ClientSideSuspense>
 			</RoomProvider>
